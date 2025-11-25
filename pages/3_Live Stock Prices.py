82d3322c5aafd1436d3af877c2c828674f79abe4
Allen Zhang <az3825@ic.ac.uk>
2025-11-25 22:25:19 +0000

    Rename input label from 'Symbol' to 'Ticker'

--- pages/3_Live Stock Prices.py
+++ pages/3_Live Stock Prices.py
@@ -8,7 +8,7 @@ st.title("📈 Live Updating Stock Price Chart")
 # refresh every 5 seconds
 st_autorefresh(interval=5000, key="chart_refresh")
 
-symbol = st.text_input("Enter Stock Symbol", "AAPL")
+symbol = st.text_input("Enter Stock Ticker", "AAPL")
 
 # get last 1 day with 1-minute interval
 data = yf.download(symbol, period="1d", interval="1m")
@@ -21,7 +21,7 @@ fig, ax = plt.subplots()
 
 ax.plot(prices.index, prices.values)
 ax.set_title(f"{symbol} Live Price")
-ax.set_xlabel("Time")
+ax.set_xlabel("Datetime")
 ax.set_ylabel("Price")
 
 st.pyplot(fig)
